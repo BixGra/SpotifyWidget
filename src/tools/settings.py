7da5cf97f3c94bd4c743d49a174b5546ef10da8a
BixGra <bixente.grandjean@gmail.com>
2024-11-16 23:10:19 +0100

    [+] Users management

--- src/tools/settings.py
+++ src/tools/settings.py
@@ -2,7 +2,7 @@ import os
 from base64 import b64encode
 
 
-base_url = os.getenv("BASE_URL")
+base_url = os.getenv("SPOTIFY_BASE_URL")
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 client_encoded = b64encode(f"{client_id}:{client_secret}".encode('utf-8')).decode('utf-8')
